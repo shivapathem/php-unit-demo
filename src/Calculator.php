59d426d47d9dfb855110eb50eebe76b7d9fe8e1a
shivapathem <shiva.pathem.ext@bbc.co.uk>
2026-03-28 22:42:06 +0530

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -11,4 +11,17 @@ class Calculator
     {
         return $a - $b;
     }
+ 
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+ 
+    public function divide($a, $b)
+    {
+        if ($b == 0) {
+            throw new Exception("Division by zero");
+        }
+        return $a / $b;
+    }
 }
