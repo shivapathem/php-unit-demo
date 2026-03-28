1217551fa7ca82c50aa1eb5e2b7a247581ba6df5
shivapathem <shiva.pathem.ext@bbc.co.uk>
2026-03-28 21:42:24 +0530

    Create CalculatorTest.php

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,20 @@
+<?php
+ 
+use PHPUnit\Framework\TestCase;
+ 
+require_once __DIR__ . '/../src/Calculator.php';
+ 
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(5, $calc->add(2, 3));
+    }
+ 
+    public function testSubtract()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(1, $calc->subtract(3, 2));
+    }
+}
