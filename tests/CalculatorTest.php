e15a008325b2417989428021d053692c04c56b39
shivapathem <shiva.pathem.ext@bbc.co.uk>
2026-03-29 13:43:28 +0530

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -17,4 +17,23 @@ class CalculatorTest extends TestCase
         $calc = new Calculator();
         $this->assertEquals(1, $calc->subtract(3, 2));
     }
+ 
+    public function testMultiply()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(6, $calc->multiply(2, 3));
+    }
+ 
+    public function testDivide()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(2, $calc->divide(4, 2));
+    }
+ 
+    public function testDivideByZero()
+    {
+        $this->expectException(Exception::class);
+        $calc = new Calculator();
+        $calc->divide(5, 0);
+    }
 }
